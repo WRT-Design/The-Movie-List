36cea60a157b86d3ce96b34df2b603cffef17a73
William Thackeray <will_thack@comcast.net>
2024-04-30 07:13:19 -0600

    updated config

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://107.20.8.183:8080",
+        target: "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
